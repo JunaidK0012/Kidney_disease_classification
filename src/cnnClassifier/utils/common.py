a0a36fc4c308dcfcef983d8f1e88a255c6fcf92b
Junaid Khan <khanj3991@gmail.com>
2024-03-21 22:43:11 +0530

    prepare base model

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -29,7 +29,6 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            print("loaded")
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
